f0e2de62d230a2a59a725f9cf121c7086562f544
AFCMS <afcm.contact@gmail.com>
2022-06-07 14:29:04 +0000

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-// https://random-word-api.herokuapp.com/word?length=5
 
 function App() {
 	let [curentWord, setCurentWord] = useState("aaaaa");
 
+	let [input, setInput] = useState([]);
+
 	useEffect(() => {
 		fetch(process.env.REACT_APP_WORD_API_URL)
-		.then(r => r.text())
-		.then(r => setCurentWord(r))
+			.then(r => r.json())
+			.then(r => setCurentWord(r))
 	}, []);
 
 	return (
 		<div className="App">
 			{curentWord}
+			<div className="bg-slate-600 h-20 w-20"></div>
+			<div className="flex justify-center align-middle">
+				<div className="">
+					<div className="flex flex-row">
+						<div className="case">A</div>
+						<div className="case">A</div>
+						<div className="case">A</div>
+						<div className="case">A</div>
+						<div className="case">A</div>
+					</div>
+					<div className="flex flex-row">
+						<div className="case">B</div>
+						<div className="case">B</div>
+						<div className="case">B</div>
+						<div className="case">B</div>
+						<div className="case">B</div>
+					</div>
+				</div>
+			</div>
+			<input type="text" name="input" id="input" className="bg-slate-200"/>
 		</div>
 	);
 }
