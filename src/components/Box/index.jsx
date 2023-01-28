650080db574ea6218bfe35b26fa313496d4d2202
AFCMS <afcm.contact@gmail.com>
2023-01-28 23:22:45 +0100

    Better Box component

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -1,15 +1,24 @@
 import "./Box.css";
 
+/**
+ * @param {{t: "edit"|"right"|"wrong"|"wplaced", letter: string}} props
+ */
 function Box(props) {
 	let case_type = "";
-	if (props.t === "edit") {
-		case_type = "case-b";
-	} else if (props.t === "right") {
-		case_type = "case-g";
-	} else if (props.t === "wrong") {
-		case_type = "case-e";
-	} else if (props.t === "wplaced") {
-		case_type = "case-y";
+
+	switch (props.t) {
+		case "edit":
+			case_type = "case-b";
+			break;
+		case "right":
+			case_type = "case-g";
+			break;
+		case "wrong":
+			case_type = "case-e";
+			break;
+		case "wplaced":
+			case_type = "case-y";
+			break;
 	}
 
 	return <div className={"case " + case_type}>{props.letter}</div>;
