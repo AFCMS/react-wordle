985d3256c67fc2a6589b6526ce60d4aca61d0b2b
AFCMS <afcm.contact@gmail.com>
2023-01-30 15:17:17 +0100

    Fix React warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
 import { useState, useEffect } from "react";
 import Box from "./components/Box";
 
-function build_row(curent_word: string | undefined, row: string, id: any) {
+/**
+ * @param curent_word The word to guess, used to determine the color of the letters
+ * @param row The 5 letter string of the line
+ * @param id Used to give boxes a unique key
+ */
+function build_row(
+	curent_word: string | undefined,
+	row: string,
+	id: number
+): Array<JSX.Element> {
 	let out = [];
 
 	if (row === undefined) {
 		for (let i = 0; i < 5; i++) {
-			out.push(<Box letter=" " key={i} />);
+			let box_id = i + 5 * id;
+			out.push(<Box letter=" " key={box_id} />);
 		}
 	} else {
 		for (let i = 0; i < 5; i++) {
+			let box_id = i + 5 * id;
 			if (row[i] === undefined) {
-				out.push(<Box letter=" " key={i} />);
+				out.push(<Box letter=" " key={box_id} />);
 			} else {
 				let char = row[i].toUpperCase();
 				let char_c =
 					curent_word === undefined ? "" : curent_word[i].toUpperCase();
 
 				if (char === char_c) {
-					out.push(<Box letter={char} key={i} t="right" />);
+					out.push(<Box letter={char} key={box_id} t="right" />);
 				} else {
 					if (curent_word === undefined ? false : curent_word.match(row[i])) {
-						out.push(<Box letter={char} key={i} t="wplaced" />);
+						out.push(<Box letter={char} key={box_id} t="wplaced" />);
 					} else {
-						out.push(<Box letter={char} key={i} t="wrong" />);
+						out.push(<Box letter={char} key={box_id} t="wrong" />);
 					}
 				}
 			}
 		}
 	}
-	return <>{out}</>;
+	return out;
 }
 
 function App() {
@@ -59,8 +70,6 @@ function App() {
 			setNewWord(false);
 		}
 	}, [newWord]);
-
-	//setCurentWord(words.at(1));
 
 	function push_rows() {
 		if (!hasWon() && !hasLost() && input.length === 5) {
